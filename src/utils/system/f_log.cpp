3244df892e4c3fee0be2335cda6a8ee3371f9277
ftd.wild <ftd.wild@gmail.com>
2020-09-05 15:04:09 +0800

    [perf,add]完善日志打印，并添加日志打印基础测试模块

--- src/utils/system/f_log.cpp
+++ src/utils/system/f_log.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "f_log.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+
+namespace ftdwild30 {
+
+#define RESET ("\033[0m")
+#define RED ("\033[31m")
+#define GREEN ("\033[32m")
+#define YELLOW ("\033[33m")
+
+
+static const char *LOG_LEVEL_NAME[] = {"ERROR  ", "WARN   ", "INFO   ", "DEBUG  ", "VERBOSE"};
+static const char *LOG_LEVEL_COLOR[] = {RED, RED, YELLOW, GREEN, GREEN};
+
+void log_cb(unsigned int level, const char *file_name, int line, const char *format, ...) {
+    //TODO,打印权限控制
+    if (level > sizeof(LOG_LEVEL_NAME) / sizeof(LOG_LEVEL_NAME[0])) {
+        level = 0;
+    }
+
+    fprintf(stdout, "%s[%s] (%s:%d) :%s", LOG_LEVEL_COLOR[level], LOG_LEVEL_NAME[level], file_name, line, RESET);
+    va_list arg;
+    va_start(arg, format);
+    vfprintf(stdout, format, arg);
+    va_end(arg);
+    fprintf(stdout, "\n");
+    fflush(stdout);
+}
+
+} // namespace ftdwild30
--- src/utils/system/f_log.h
+++ src/utils/system/f_log.h
@@ -5,15 +5,33 @@
 #ifndef NETWORK_BASE_F_LOG_H
 #define NETWORK_BASE_F_LOG_H
 
-#include <stdio.h>
+#include <string.h>
+
+/**
+ * @notice : 这是一个简易的日志打印实现。
+ *           若需要添加日志打印的各项功能，推荐在遵守zlog开源协议的前提下，使用zlog。
+ *
+ */
 
 namespace ftdwild30 {
 
-#define LOG_ERROR printf
-#define LOG_WARN printf
-#define LOG_INFO printf
-#define LOG_DEBUG printf
-#define LOG_VERBOSE printf
+enum LOG_LEVEL {
+    LOG_LEVEL_ERROR = 0,
+    LOG_LEVEL_WARN,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_DEBUG,
+    LOG_LEVEL_VERBOSE,
+};
+
+void log_cb(unsigned int level, const char *file_name, int line, const char *format, ...);
+
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+#define LOG_ERROR(args...) log_cb(LOG_LEVEL_ERROR, __FILENAME__, __LINE__, args)
+#define LOG_WARN(args...) log_cb(LOG_LEVEL_WARN, __FILENAME__, __LINE__, args)
+#define LOG_INFO(args...) log_cb(LOG_LEVEL_INFO, __FILENAME__, __LINE__, args)
+#define LOG_DEBUG(args...) log_cb(LOG_LEVEL_DEBUG, __FILENAME__, __LINE__, args)
+#define LOG_VERBOSE(args...) log_cb(LOG_LEVEL_VERBOSE, __FILENAME__, __LINE__, args)
 
 
 } // namespace ftdwild30
