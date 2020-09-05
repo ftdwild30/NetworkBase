79e590be857d0b061e2b944abf7cf85c00a63599
ftd.wild <ftd.wild@gmail.com>
2020-09-05 14:31:37 +0800

    [add]添加简易日志函数

---
+++ src/utils/system/f_log.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ftd.wild on 2020/9/5.
+//
+
+#include "f_log.h"
---
+++ src/utils/system/f_log.h
@@ -0,0 +1,22 @@
+//
+// Created by ftd.wild on 2020/9/5.
+//
+
+#ifndef NETWORK_BASE_F_LOG_H
+#define NETWORK_BASE_F_LOG_H
+
+#include <stdio.h>
+
+namespace ftdwild30 {
+
+#define LOG_ERROR printf
+#define LOG_WARN printf
+#define LOG_INFO printf
+#define LOG_DEBUG printf
+#define LOG_VERBOSE printf
+
+
+} // namespace ftdwild30
+
+
+#endif //NETWORK_BASE_F_LOG_H
